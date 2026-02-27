d7e84132c3731f2c747a4980de34bdef807b372b
Theone Eclarin <theoneeclarin@outlook.com>
2026-02-27 13:30:55 +0800

    feat: add tanstack router & shadcn/ui

--- packages/app-web/vite.config.ts
+++ packages/app-web/vite.config.ts
@@ -1,7 +1,25 @@
+import path from "path";
+import tailwindcss from "@tailwindcss/vite";
+import { tanstackRouter } from "@tanstack/router-plugin/vite";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-	plugins: [react()],
+	plugins: [
+		tanstackRouter({
+			target: "react",
+			autoCodeSplitting: true,
+		}),
+		react(),
+		tailwindcss(),
+	],
+	resolve: {
+		alias: [
+			{
+				find: /^@\//,
+				replacement: `${path.resolve(__dirname, "./src")}/`,
+			},
+		],
+	},
 });
